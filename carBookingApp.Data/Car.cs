21fa845b53d6994e8e158730a356521734790b17
jerome <zachariah11jerobert@gmail.com>
2024-03-12 13:28:05 +0530

    EntityFrameworkCore Dependency Added and Db Context created

---
+++ carBookingApp.Data/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace carBookingApp.Data
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public int Year { get; set; }
+
+        public string Name { get; set; }
+    }
+}
